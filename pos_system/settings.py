5d0a5b66cce69e27a5d43639e4d04e7c097e48c4
Drew Hilario <72259687+andrewhilario@users.noreply.github.com>
2023-01-13 08:41:22 +0800

    Update settings.py

--- pos_system/settings.py
+++ pos_system/settings.py
@@ -15,15 +15,15 @@ import os
 import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-9n@d6h9wo%1hul)$m++5d!mymt1yt5rmg!&2n^0m*oie_clsk5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
